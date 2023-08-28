7d5fb221460c5c07818a54d43b0c0ccb66124d16
Igor Gavrilyuk <88338015+GavrilyukII@users.noreply.github.com>
2023-08-28 10:24:49 +0300

    Delete Linux_to_python-master directory

--- Linux_to_python-master/lesson_02/stdinout.py
+++
@@ -1,5 +0,0 @@
-import subprocess
-
-result = subprocess.run(['ping', '-c', '3', '-n', 'yandex.ru'], encoding='utf-8', stderr=subprocess.STDOUT,
-                        stdout=subprocess.PIPE)
-print(result.stdout)
--- Linux_to_python-master/main.py
+++
@@ -1,6 +0,0 @@
-def print_hi(name: object):
-    print(f'Hi, {name}')
-
-
-if __name__ == '__main__':
-    print('PyCharm')
